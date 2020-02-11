a1a92efc7f9f453440cf6418008cfc72a307878f
FelipeDecome <felipe.decome@hotmail.com>
2020-02-10 21:06:43 -0300

    css sprites teste

--- layouts _Simples/components/slideshows/js/slider.js
+++ layouts _Simples/components/slideshows/js/slider.js
@@ -4,9 +4,9 @@ const sliderItems = slideWrapper.children;
 const sliderControls = document.querySelector(".control-list");
 
 const slideDelay = 1000;
-const slideSplits = 12;
-const imageWidth = 1024;
-const imageHeight = 720;
+const slideSplits = 5;
+const imageWidth = 960;
+const imageHeight = 540;
 
 for (let i = 0; i < sliderItems.length; i++) {
   let li = document.createElement("li");
@@ -16,16 +16,43 @@ for (let i = 0; i < sliderItems.length; i++) {
 }
 
 var image = {
-  id: 0,
-  element: document.querySelector("#image1"),
+  element: document.querySelector("#image4"),
   width: imageWidth,
   height: imageHeight,
-  swidth: imageWidth / 12,
-  sheight: imageHeight / 12,
-  get sx() {
-    return this.swidth * this.id;
-  },
-  get sy() {
-    return this.sheight * this.id;
-  }
+  swidth: imageWidth / slideSplits,
+  sheight: imageHeight / slideSplits,
 };
+
+const div = document.querySelector('.teste');
+
+
+for (let row = slideSplits; row > 0; row--) {
+
+  for (let column = slideSplits; column > 0; column--) {
+
+    let span = document.createElement('span');
+    span.setAttribute('name', `${row}-${column}`);
+    span.style.background = `url(${image.element.src}) ${image.swidth * column}px ${image.sheight * row}px `;
+    span.style.height = `${image.sheight}px`;
+    span.style.width = `${image.swidth}px`;
+
+    div.append(span);
+
+    // console.log(image.id);
+  }
+}
+
+for (let row = 0; row < slideSplits; row++) {
+  for (let column = 0; column < slideSplits; column++) {
+
+    let span = document.createElement('span');
+    span.setAttribute('name', `${row}-${column}`);
+    span.style.background = `url(${image.element.src}) -${image.swidth * column}px -${image.sheight * row}px `;
+    span.style.height = `${image.sheight}px`;
+    span.style.width = `${image.swidth}px`;
+
+    div.append(span);
+
+    // console.log(image.id);
+  }
+}
