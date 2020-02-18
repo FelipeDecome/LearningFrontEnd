0ea7a5078053454b442e4a2581caf540a73556c5
FelipeDecome <felipe.decome@hotmail.com>
2020-02-17 22:22:59 -0300

    bootstrap testes

---
+++ bootstrap 4/view/components/clientsCards/index.php
@@ -0,0 +1,22 @@
+<div class="card bg-white-radial shadow-sm rounded mx-2">
+  <div class="card-body">
+    <h4 class="card-title">João Pedro da Silva</h4>
+    <h6 class="card-text lead">
+      Rua do Trabalhador 15, Jardim Triunfo.
+      <br />
+      Pedreira - SP
+    </h6>
+
+    <ul class="list-unstyled ml-3 text-muted">
+      <li>(19) 3893 - 4121</li>
+      <li>(19) 99618 - 2095</li>
+    </ul>
+
+    <div class="btn-group float-right" role="group" aria-label="Basic example">
+      <button type="button" class="btn btn-link text-info">
+        Orçamentos <span class="badge badge-dark">0</span>
+      </button>
+      <a href="#" class="btn btn-danger stretched-link">Deletar</a>
+    </div>
+  </div>
+</div>
